1b444d8a0f1fae77b39f91d55fd7057bb5c8f1c0
wuchuanming <wuchuanming1990@gmail.com>
2024-10-18 16:16:10 +0800

    add cron job

--- main.go
+++ main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// 创建 Gin 引擎
 	r := gin.Default()
 
-	// 静态文件路由
+	// 设置静态文件夹
 	r.Static("/static", "./static")
 
-	// 路由处理
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "My Gin App",
-		})
-	})
-
-	// 加载模板文件
+	// 设置模板文件夹
 	r.LoadHTMLGlob("templates/*")
 
-	// 启动服务
+	// 路由
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+	})
+
+	// 初始化并启动定时任务
+	StartScheduler()
+
+	// 启动 Gin 服务
 	r.Run(":8080")
 }
